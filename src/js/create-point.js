5a24a6d8b4f27f8a83ef3b6570c63bb225047ec3
Kbath2 <kbjass2@gmail.com>
2020-06-07 14:47:28 -0300

    Finalização da pag de cadastro

--- src/js/create-point.js
+++ src/js/create-point.js
@@ -36,9 +36,39 @@ function getCities(event) {
 
 }
 
-
-
-
 document
     .querySelector("select[name=uf")
     .addEventListener("change", getCities)
+
+// JS do form de Itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    // add ou remover uma class
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    // Verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+        // se já tiver selecionado, tirar da seleção
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => item != itemId)
+
+        selectedItems = filteredItems
+    } else {
+        // se nao tiver selecionado, adcionar a seleção
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
