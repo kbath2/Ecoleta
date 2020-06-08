352bea4b5af864399d3ea900ae75442922b3ee00
Kbath2 <kbjass2@gmail.com>
2020-06-08 14:48:27 -0300

    Configuração do template engine

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const server = express()
 
+// configuração da pasta publica
 server.use(express.static("public"))
 
+// Configuração da template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// Rotas 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
