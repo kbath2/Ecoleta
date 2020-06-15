2eb7a476fc9c305d3f0aba2d0aa72f4e63d6945a
Kbath2 <kbjass2@gmail.com>
2020-06-15 20:23:18 -0300

    Img para o readme

--- src/database/db.js
+++ src/database/db.js
@@ -58,13 +58,13 @@ db.serialize(() => {
     //		 console.log(rows)
     //	})
 
-    // db.run(`DELETE FROM places WHERE id = ?`, [4], function(err) {
-    //   if (err) {
+    // db.run(`DELETE FROM places WHERE id = ?`, [0], function(err) {
+    // if (err) {
     //     return console.log(err)
     // }
 
-    //   console.log("Registro deletado com sucesso!")
-    //})
+    //  console.log("Registro deletado com sucesso!")
+    // })
 
 })
 
