c111970357b7d753678ad33fe300b626da39a293
Kbath2 <kbjass2@gmail.com>
2020-06-15 14:17:02 -0300

    configuração do salvamento dos dados no bd

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const db = require("./database/db")
 
 // configuração da pasta publica
 server.use(express.static("public"))
+    // habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // Configuração da template engine
 const nunjucks = require("nunjucks")
@@ -18,12 +20,54 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+
+
     return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+    const query = `
+            INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+            ) VALUES (?,?,?,?,?,?,?);
+            return res.send("ok")
+        })`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
 })
 
 server.get("/search", (req, res) => {
 
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`
+            SELECT * FROM places `, function(err, rows) {
         if (err) {
             return console.log(err)
         }
